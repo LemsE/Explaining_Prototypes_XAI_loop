791b7a0086f8333b79c6d68606997301dc8b5f01
LemsE <60966044+LemsE@users.noreply.github.com>
2023-05-20 16:32:08 +0200

    Update crop_images.py
    Updates such that problematic images are not added in the train/test folders

--- preprocessing/crop_images.py
+++ preprocessing/crop_images.py
@@ -1,18 +1,20 @@
 import cv2
 import os
+import numpy as np
 
-boxes = open("./CUB_200_2011/CUB_200_2011/bounding_boxes.txt")
-images_id = open("./CUB_200_2011/CUB_200_2011/images.txt")
-train_test = open("./CUB_200_2011/CUB_200_2011/train_test_split.txt")
-folder = "./CUB_200_2011/CUB_200_2011/images/"
-path_test = "./CUB_200_2011/CUB_200_2011/datasets/cub200_cropped/test_cropped/"
-path_training = "./CUB_200_2011/CUB_200_2011/datasets/cub200_cropped/train_cropped/"
+boxes = open("./data/CUB_200_2011/bounding_boxes.txt")
+images_id = open("./data/CUB_200_2011/images.txt")
+train_test = open("./data/CUB_200_2011/train_test_split.txt")
+folder = "./data/CUB_200_2011/images/"
+path_test = "./data/CUB_200_2011/datasets/cub200_cropped/test_cropped/"
+path_training = "./data/CUB_200_2011/datasets/cub200_cropped/train_cropped/"
 
-
+# CSV containing all problematic samples
+loaded_problematic_samples = np.loadtxt('problematic_samples.csv', delimiter=',', dtype=str )
 # Make directory if it does not exist
 def makedir(path):
     if not os.path.exists(path):
-        os.makedirs(path)
+        os.makedirs(path)   
 
 
 for (line, box, train) in zip(images_id, boxes, train_test):
@@ -28,11 +30,16 @@ for (line, box, train) in zip(images_id, boxes, train_test):
             coords[1]:coords[1] + coords[3], coords[0]:coords[0] + coords[2], :]
 
     # Store image either as train or test image
-    if is_train:
+    if is_train and not(image_name in loaded_problematic_samples):
         path_folder = os.path.join(path_training, image_folder)
         makedir(os.path.join(path_training, image_folder))
-    else:
+    elif not (is_train) and not(image_name in loaded_problematic_samples):
         path_folder = os.path.join(path_test, image_folder)
         makedir(os.path.join(path_test, image_folder))
-    cv2.imwrite(os.path.join(path_folder, image_name).replace('jpg', 'JPEG'), image)
+    else:
+        print("problematic samples skipped in train test split: ", image_name)
+    if not(image_name in loaded_problematic_samples):
+      cv2.imwrite(os.path.join(path_folder, image_name).replace('jpg', 'JPEG'), image)
+    else:
+        print("problematic samples skipped: ", image_name)
 
