5d91b5bc14bc879a9a5634d7afcfa77009c3e172
LemsE <60966044+LemsE@users.noreply.github.com>
2023-05-20 17:30:58 +0200

    Updated settings.py
    Updated some settings

--- settings.py
+++ settings.py
@@ -1,13 +1,13 @@
-base_architecture = 'vgg19'
+base_architecture = 'resnet50'
 img_size = 224
 prototype_shape = (2000, 128, 1, 1)
 num_classes = 200
 prototype_activation_function = 'log'
 add_on_layers_type = 'regular'
 
-experiment_run = '003'
+experiment_run = '000'
 
-data_path = './datasets/cub200_cropped/'
+data_path = 'data/CUB_200_2011/datasets/cub200_cropped/'
 train_dir = data_path + 'train_cropped_augmented/'
 test_dir = data_path + 'test_cropped/'
 train_push_dir = data_path + 'train_cropped/'
